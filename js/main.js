014f048c955dfecd212e5a552b42cce65421b5d7
ahsazzad <71392727+ahsazzad@users.noreply.github.com>
2020-09-27 18:44:03 +0600

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,39 @@
+// Initialize and add the map
+function initMap() {
+  // Your location
+  const loc = { lat: 23.707310, lng: 90.415482 };
+  // Centered map on location
+  const map = new google.maps.Map(document.querySelector('.map'), {
+    zoom: 14,
+    center: loc
+  });
+  // The marker, positioned at location
+  const marker = new google.maps.Marker({ position: loc, map: map });
+}
+
+// Smooth Scrolling
+$('#navbar a, .btn').on('click', function(event) {
+  if (this.hash !== '') {
+    event.preventDefault();
+
+    const hash = this.hash;
+
+    $('html, body').animate(
+      {
+        scrollTop: $(hash).offset().top - 100
+      },
+      800
+    );
+  }
+});
+
+
+// Sticky menu background
+window.addEventListener('scroll', function() {
+  if (window.scrollY > 150) {
+    document.querySelector('#navbar').style.opacity = 0.9;
+  } else {
+    document.querySelector('#navbar').style.opacity = 1;
+  }
+});
+
